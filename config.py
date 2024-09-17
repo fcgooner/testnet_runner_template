c9b3d6cd86e651f363aec3260cc1a0f9b35eeab7
fcgooner <r.maniachuk@gmail.com>
2024-09-18 02:46:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ WALLET_NAMES = [        # List of wallets to unlock before executing tasks
 # TESTNET SETTINGS
 
 TESTNET_TASKS_DATAFILES = {             # Path to testnet tasks CSV-table
-    "TESTNET 1": "testnets/first_testnet/testnet_data/testnet1_tasks_data.csv",
-    "TESTNET 2": "testnets/second_testnet/testnet_data/testnet2_tasks_data.csv",
+    "TESTNET1": "testnets/first_testnet/testnet_data/testnet1_tasks_data.csv",
+    "TESTNET2": "testnets/second_testnet/testnet_data/testnet2_tasks_data.csv",
 }
 
 CRITICAL_TASKS = ["TESTNET1 TASK1"]     # List of tasks, without the successful completion of which there is no point
@@ -33,7 +33,7 @@ CRITICAL_TASKS = ["TESTNET1 TASK1"]     # List of tasks, without the successful 
 TESTNET_TASKS = {
 
     # TESTNET 1
-    "TESTNET 1": {
+    "TESTNET1": {
         'CORE': [               # Tasks, that are executed first, execution order not shuffled
             "TESTNET1 TASK1",
         ],
@@ -48,7 +48,7 @@ TESTNET_TASKS = {
     },
 
     # TESTNET 2
-    "TESTNET 2": {
+    "TESTNET2": {
         'CORE': [
                 "TESTNET2 TASK1",
                 "TESTNET2 TASK2"
